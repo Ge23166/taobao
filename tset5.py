f4c3122c4cc3c934a9e46e1562030e9e462a6703
Ge23166 <ga1985ti@163.com>
2025-11-02 10:22:12 +0800

    Updatetset5.py

--- tset5.py
+++ tset5.py
@@ -1,9 +1,8 @@
 mylist = ['blue','orange','green']
 mapped_dict = (zip(itr, map(fn, itr)))
 print(mapped_dict)
-
-def function():
-    """开发一半的代码"""
+print("what is amend")
+print("what is amend")
 
 print("第一部分")
 print("第二部分")
